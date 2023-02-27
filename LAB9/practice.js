404489af7a5f3d0326cdc8d9a1519751a0982e8c
Abhishek Joshi <96511247+JoshiAbhishek866@users.noreply.github.com>
2023-02-27 11:33:19 +0530

    Create practice.js

---
+++ LAB9/practice.js
@@ -0,0 +1,30 @@
+let arr =[
+{
+    "st1":"abhi",
+    "id":"52",
+    "marks1":"99",
+    "marks2":"100",
+    "marks3":"98",
+},
+ {
+        "st2":"dev",
+        "id":"111",
+        "marks1":"90",
+        "marks2":"85",
+        "marks3":"70",
+ },
+ {
+    "st3":"ashwin",
+    "id":"192",
+    "marks1":"85",
+    "marks2":"58",
+    "marks3":"80",
+}
+];
+array.forEach((element,i) => {
+  //var per[i]=(arr[i].marks1+arr[i].marks2+arr[i].marks3)/3*100;
+const table="<tr>";
+  let sum=()
+    
+});//create element fun and create apppend fun
+ 
